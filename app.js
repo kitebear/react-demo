6d0d83b18b0c75a18aa11e6025ebd2a41019746d
dhxie <xie5721072@163.com>
2016-02-01 18:57:29 +0800

    图标

--- app.js
+++ app.js
@@ -1,21 +1,28 @@
-import Koa    from 'koa'
-import logger from 'koa-logger'
-import bodyParser from 'koa-bodyparser'
-import koa_static from 'koa-static'
-import koa_json from 'koa-json'
-import path   from 'path'
+import Koa          from 'koa'
+import logger       from 'koa-logger'
+import bodyParser   from 'koa-bodyparser'
+import koa_static   from 'koa-static'
+import koa_json     from 'koa-json'
+import path         from 'path'
 import koa_static_cache from 'koa-static-cache'
-import views  from 'koa-views'
+import views        from 'koa-views'
+import favicon      from 'koa-favicon'
 
 const PATH = __dirname + "/" || path.join("./");
 
 const app       = new Koa();
 const router    = require(PATH + 'routes/index.js');
 
+app.use(favicon(__dirname + '/public/favicon.ico'));
+
 app.use(views('views', {
     root: __dirname + '/views',
     default: 'jade'
 }));
+
+app.use(koa_static_cache(path.join(__dirname, 'public'), {
+    maxAge: 600000 * 60
+}))
 
 // 中间件
 app.use(bodyParser());
@@ -24,10 +31,6 @@ app.use(koa_json());
 if(!process.env.NODE_ENV != 'production'){
     app.use(logger());
 }
-
-app.use(koa_static_cache(path.join(__dirname, 'public'), {
-    maxAge: 600000 * 60
-}))
 
 app.use(koa_static(__dirname + '/public'));
 
