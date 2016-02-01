69e2b8946659e3ae8ae1ff94ec416aad2c82c543
dhxie <xie5721072@163.com>
2016-02-01 18:30:24 +0800

    ( ⊙ o ⊙ )啊

--- app.js
+++ app.js
@@ -1,19 +1,35 @@
 import Koa    from 'koa'
 import logger from 'koa-logger'
 import bodyParser from 'koa-bodyparser'
+import koa_static from 'koa-static'
+import koa_json from 'koa-json'
 import path   from 'path'
+import koa_static_cache from 'koa-static-cache'
+import views  from 'koa-views'
 
 const PATH = __dirname + "/" || path.join("./");
 
-const app           = new Koa();
-const router        = require(PATH + 'routes/index.js');
+const app       = new Koa();
+const router    = require(PATH + 'routes/index.js');
+
+app.use(views('views', {
+    root: __dirname + '/views',
+    default: 'jade'
+}));
 
 // 中间件
+app.use(bodyParser());
+app.use(koa_json());
+
 if(!process.env.NODE_ENV != 'production'){
     app.use(logger());
 }
 
-app.use(bodyParser());
+app.use(koa_static_cache(path.join(__dirname, 'public'), {
+    maxAge: 600000 * 60
+}))
+
+app.use(koa_static(__dirname + '/public'));
 
 // 响应
 app.use(router.routes()).use(router.allowedMethods());
