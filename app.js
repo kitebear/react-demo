ff85cb59973117b17d6344f0be31dc6c975594e8
dhxie <xie5721072@163.com>
2016-02-05 15:11:10 +0800

    图标

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
-import Koa          from 'koa'
-import logger       from 'koa-logger'
-import bodyParser   from 'koa-bodyparser'
-import koa_static   from 'koa-static'
-import koa_json     from 'koa-json'
-import path         from 'path'
-import koa_static_cache from 'koa-static-cache'
-import views        from 'koa-views'
-import favicon      from 'koa-favicon'
+import Koa              from 'koa'
+import logger           from 'koa-logger'
+import bodyParser       from 'koa-bodyparser'
+import koaStatic        from 'koa-static'
+import koaJson          from 'koa-json'
+import path             from 'path'
+import koaStaticCache   from 'koa-static-cache'
+import views            from 'koa-views'
+import favicon          from 'koa-favicon'
+import webpack          from 'webpack'
+import webpackConfig    from './webpack.config'
 
 const PATH = __dirname + "/" || path.join("./")
 
 const app       = new Koa();
 const router    = require(PATH + 'routes/index.js')
+const compiler  = webpack(webpackConfig);
 
 app.use(favicon(__dirname + '/public/favicon.ico'))
 
@@ -20,27 +23,41 @@ app.use(views('views', {
     default: 'html'
 }))
 
-app.use(koa_static_cache(path.join(__dirname, 'public'), {
+app.use(koaStaticCache(path.join(__dirname, 'public'), {
     maxAge: 600000 * 60
 }))
 
 // 中间件
 app.use(bodyParser())
-app.use(koa_json())
+app.use(koaJson())
 
 if(!process.env.NODE_ENV != 'production'){
     app.use(logger())
+
+    compiler.watch({
+        aggregateTimeout: 300,
+        poll: true
+    }, function(err, stats) {
+        if(err)
+            console.log(err.message);
+        var jsonStats = stats.toJson();
+        if(jsonStats.errors.length > 0)
+            console.log(jsonStats.errors);
+        if(jsonStats.warnings.length > 0)
+            console.log(jsonStats.warnings);
+        console.log('compiler complete');
+    });
 }
 
-app.use(koa_static(__dirname + '/public'))
+app.use(koaStatic(__dirname + '/dist'))
 
 // 响应
 app.use(router.routes()).use(router.allowedMethods())
 
 // 错误
 app.on('error', function(err, ctx){
-    log.error('server error', err, ctx)
-})
+    console.log.error('server error', err, ctx)
+});
 
 // 监听3000
 app.listen(3000,err => {
---
+++ public/entry.js
@@ -0,0 +1,4 @@
+console.log(1);
+import React from 'react'
+
+console.log(React);
